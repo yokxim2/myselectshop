727bd56947d4de6cd41a0feb4e0d4196d80c8139
yokxim <yooksi53@gmail.com>
2024-10-12 21:09:53 +0900

    Product paging and listing

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -2,12 +2,12 @@ package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByUser(User user);
+    Page<Product> findAllByUser(User user, Pageable pageable);
 }
