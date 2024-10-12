5f3e53d868be9a29a0fe195beae42631f54f4fbc
yokxim <yooksi53@gmail.com>
2024-10-12 19:37:43 +0900

    WishList Registration

---
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
