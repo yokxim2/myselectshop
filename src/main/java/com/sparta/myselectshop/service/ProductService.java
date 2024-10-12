e181ed77807ca20aa5221c7069b4a7a535a292b1
yokxim <yooksi53@gmail.com>
2024-10-12 22:23:15 +0900

    Listup products by each folder

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -105,6 +105,19 @@ public class ProductService {
         productFolderRepository.save(new ProductFolder(product, folder));
     }
 
+    @Transactional
+    public Page<ProductResponseDto> getProductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc, User user) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<Product> productList = productRepository.findAllByUserAndProductFolderList_FolderId(user, folderId, pageable);
+
+        Page<ProductResponseDto> responseDtoList = productList.map(ProductResponseDto::new);
+
+        return responseDtoList;
+    }
+
     private boolean validateProductAndFolderByUser(Product product, Folder folder, User user) {
         if (!product.getUser().getId().equals(user.getId())
                 || !folder.getUser().getId().equals(user.getId())) {
